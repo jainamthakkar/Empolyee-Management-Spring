f7cd93a3512abb3ceeed50d2184aa1fa1ca8a332
jainamthakkar <jainamthakkar111@gmail.com>
2023-09-08 23:10:20 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController{
+	
+	@GetMapping("/") 
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/loadEmpSave") 
+	public String loadEmpSave() {
+		return "emp_save";
+	}
+	
+	@GetMapping("/edit") 
+	public String editEmp() {
+		return "edit";
+	}
+}
---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,71 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String  name, email, password, address, gender;
+	
+	public Employee(int id, String email, String password, String name, String address, String gender) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.address = address;
+		this.gender = gender;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", email=" + email + ", password=" + password + ", name=" + name + ", address="
+				+ address + ", gender=" + gender + "]";
+	}
+	
+	
+}
