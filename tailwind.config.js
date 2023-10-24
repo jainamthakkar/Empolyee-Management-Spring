3adf6801097da8ad1a19f44d749f31ece469e54c
jainamthakkar <jainamthakkar111@gmail.com>
2023-10-24 11:59:24 +0530

    added postcss and version=51 in css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-	  "./src/main/resources/templates/**/*.html"
+    "./src/main/resources/templates/**/*.html"
   ],
   theme: {
     extend: {},
